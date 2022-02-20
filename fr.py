408e1f6cb5a2b5888f93ada5fb8e2ae70cc1f3fb
Morgan Joyce <84004324+mojo-py@users.noreply.github.com>
2022-02-20 15:57:43 -0600

    making it clear this is the API endpoint

--- fr.py
+++ fr.py
@@ -6,8 +6,8 @@ import requests
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-url = "https://www.flightradar24.com/flights/most-tracked"
-r = requests.get(url, headers=headers)
+api_endpoint = "https://www.flightradar24.com/flights/most-tracked"
+r = requests.get(api_endpoint, headers=headers)
 
 j = json.loads(r.text)
 df = pd.DataFrame(j['data'])
