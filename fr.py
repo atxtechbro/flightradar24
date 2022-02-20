75bc5d2000c923a0cc5bfb2f40199c66271f2dba
Morgan Joyce <84004324+mojo-py@users.noreply.github.com>
2022-02-20 13:52:24 -0600

    Create fr.py
    all the code

---
+++ fr.py
@@ -0,0 +1,15 @@
+import json
+import pandas as pd
+import requests
+
+
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+url = "https://www.flightradar24.com/flights/most-tracked"
+r = requests.get(url, headers=headers)
+
+j = json.loads(r.text)
+df = pd.DataFrame(j['data'])
+df.set_index('flight_id', inplace=True)
+
+print(df)
