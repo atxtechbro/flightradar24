403462581ea5dbc206bded86f82716f72adf9e59
Morgan Joyce <mjoyce@sertifi.com>
2023-12-29 20:28:30 -0600

    update user agent in fr.py

--- fr.py
+++ fr.py
@@ -4,7 +4,11 @@ import pandas as pd
 import requests
 
 
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) '
+                  'AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/39.0.2171.95 Safari/537.36'
+}
 
 api_endpoint = "https://www.flightradar24.com/flights/most-tracked"
 r = requests.get(api_endpoint, headers=headers)
