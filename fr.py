29a922b577032927a84469c4a55771c1869aed78
Morgan Joyce <morganj2k@gmail.com>
2023-12-29 20:42:30 -0600

    make code more testable

--- fr.py
+++ fr.py
@@ -1,34 +1,31 @@
 import json
 import pandas as pd
 import requests
-import logging
 
-# Configure logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
+class FlightDataRetriever:
+    def __init__(self, api_endpoint, headers):
+        self.api_endpoint = api_endpoint
+        self.headers = headers
+
+    def retrieve_flight_data(self):
+        response = requests.get(self.api_endpoint, headers=self.headers)
+        response.raise_for_status()  # Raise HTTPError for bad responses
+
+        json_data = json.loads(response.text)
+        flight_data = pd.DataFrame(json_data.get('data', []))
+        flight_data.set_index('flight_id', inplace=True)
+
+        return flight_data
+
+
+# Usage
+api_endpoint = "https://www.flightradar24.com/flights/most-tracked"
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) '
                   'AppleWebKit/537.36 (KHTML, like Gecko) '
                   'Chrome/39.0.2171.95 Safari/537.36'
 }
 
-api_endpoint = "https://www.flightradar24.com/flights/most-tracked"
-response = requests.get(api_endpoint, headers=headers)
-
-# Log the API request status
-logger.info(f"API request status code: {response.status_code}")
-
-try:
-    json_data = json.loads(response.text)
-    flight_data = pd.DataFrame(json_data['data'])
-    flight_data.set_index('flight_id', inplace=True)
-    logger.info("Data loaded successfully")
-except json.JSONDecodeError as e:
-    logger.error(f"Error decoding JSON data: {e}")
-except KeyError as e:
-    logger.error(f"KeyError: {e}. Check the structure of the JSON response.")
-    flight_data = pd.DataFrame()
-
-# Log the number of rows and columns in the DataFrame
-logger.info(f"Number of rows: {flight_data.shape[0]}, Number of columns: {flight_data.shape[1]}")
+flight_data_retriever = FlightDataRetriever(api_endpoint, headers)
+flight_data = flight_data_retriever.retrieve_flight_data()
